b086b91a5116401d54cee5549108d4298fc780dc
Krishnavamsi <bendalamkrishnavamsi@gmail.com>
2019-10-06 20:05:56 +0530

    Final todo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
+import { TodoBanner } from "./TodoBanner";
+import { TodoCreator } from "./TodoCreator";
+import { TodoRow } from "./TodoRow";
+import { VisibilityControl } from "./VisibilityControl";
 
-
-class App extends Component {
+export default class App extends Component {
 
   constructor(props) {
     super(props);
@@ -12,7 +15,8 @@ class App extends Component {
         { action: "Wasing Clothes", done: false },
         { action: "Bathing", done: true }
       ],
-      newItemText: ''
+      showCompleted: true
+      //newItemText: ''
     }
   }
 
@@ -23,14 +27,11 @@ class App extends Component {
     })
   }
 
-  createNewTodo = () => {
-    if (!this.state.todoItems
-      .find(item => item.action === this.state.newItemText)) {
+  createNewTodo = (task) => {
+    if (!this.state.todoItems.find(item => item.action === task)) {
       this.setState({
-        todoItems: [...this.state.todoItems,
-        { action: this.state.newItemText, done: false }],
-        newItemText: ""
-      });
+        todoItems: [...this.state.todoItems, { action: task, done: false }]
+      }, () => localStorage.setItem("todos", JSON.stringify(this.state)));
     }
   }
 
@@ -41,44 +42,53 @@ class App extends Component {
         ? { ...item, done: !item.done } : item)
   });
 
-  todoTableRows = () => this.state.todoItems.map(item =>
-    <tr key={item.action}>
-      <td>{item.action}</td>
-      <td>
-        <input type="checkbox" checked={item.done}
-          onChange={() => this.toggleTodo(item)} />
-      </td>
-    </tr>
-  );
+  todoTableRows = (doneValue) => this.state.todoItems
+    .filter(item => item.done === doneValue).map(item =>
+      <TodoRow key={item.action} item={item}
+        callback={this.toggleTodo} />)
 
 
-  render() {
-    return (
-      <div>
-        <h4 className="bg-primary text-white text-center p-2">
-          {this.state.userName}'s To Do List
-          ({this.state.todoItems.filter(t => !t.done).length})
-        </h4>
+  componentDidMount = () => {
+    let data = localStorage.getItem("todos");
+    this.setState(data != null
+      ? JSON.parse(data)
+      : {
+        userName: "Adam",
+        todoItems: [{ action: "Buy Flowers", done: false },
+        { action: "Get Shoes", done: false },
+        { action: "Collect Tickets", done: true },
+        { action: "Call Joe", done: false }],
+        showCompleted: true
+      });
+  }
 
-        <div className="container-fluid">
-          <div className="my-1">
-            <input className="form-control"
-              value={this.state.newItemText}
-              onChange={this.updateNewTextValue} />
-            <button className="btn btn-primary mt-1"
-              onClick={this.createNewTodo}>Add</button>
-          </div>
 
+  render = () =>
+    <div>
+      <TodoBanner name={this.state.userName}
+        tasks={this.state.todoItems} />
+      <div className="container-fluid">
+        <TodoCreator callback={this.createNewTodo} />
+        <table className="table table-striped table-bordered">
+          <thead>
+            <tr><th>Description</th><th>Done</th></tr>
+          </thead>
+          <tbody>{this.todoTableRows(false)}</tbody>
+        </table>
+        <div className="bg-secondary text-white text-center p-2">
+          <VisibilityControl description="Completed Tasks"
+            isChecked={this.state.showCompleted}
+            callback={(checked) =>
+              this.setState({ showCompleted: checked })} />
+        </div>
+        {this.state.showCompleted &&
           <table className="table table-striped table-bordered">
             <thead>
               <tr><th>Description</th><th>Done</th></tr>
             </thead>
-            <tbody>{this.todoTableRows()}</tbody>
+            <tbody>{this.todoTableRows(true)}</tbody>
           </table>
-        </div>
-      </div >
-    );
-  }
+        }
+      </div>
+    </div>
 }
-
-export default App;
